3404d15581f774571ca7253c16d68e79db341c40
Patricio García Ocampo <patgo39@gmail.com>
2026-04-13 22:38:50 -0600

    Prototipo de login

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 from db import db
 import os
 from dotenv import load_dotenv
+from controllers.login_controller import login_bp
 
 # Cargar variables de entorno
 load_dotenv()
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 # Configuración de Aiven
 user = os.getenv('DB_USER')
@@ -18,6 +20,12 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Conectar la base de datos a la app
 db.init_app(app)
+
+app.register_blueprint(login_bp, url_prefix='/auth')
+
+@app.route('/')
+def index():
+    return redirect(url_for('auth.iniciar_sesion'))
 
 if __name__ == '__main__':
     app.run(host="127.0.0.1", port=5000, debug=True)
--- controllers/login_controller.py
+++ controllers/login_controller.py
@@ -1,10 +1,11 @@
-from flask import Blueprint, render_template, request, redirect, flash, session
+from flask import Blueprint, render_template, request, redirect, flash, session, url_for
 from dao.usuario_dao import UsuarioDao
-login_bp = Blueprint('login', __name__)
+login_bp = Blueprint('auth', __name__)
+
 @login_bp.route('/login', methods=['GET', 'POST'])
 def iniciar_sesion():
     if request.method == 'GET':
-        return render_template('LoginIH.html')
+        return render_template('auth/LoginIH.html')
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
@@ -20,7 +21,7 @@ def iniciar_sesion():
                 return redirect('/tablero_docente')
         else:
             flash("Usuario o contraseñas incorrectos. Intenta de nuevo")
-            return redirect('/login')
+            return redirect(url_for('auth.iniciar_sesion'))
 
 @login_bp.route('/tablero_alumno')
 def tablero_alumno():
