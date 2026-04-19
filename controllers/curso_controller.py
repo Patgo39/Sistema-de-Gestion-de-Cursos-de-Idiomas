417e17d18abf2510de6b9f5a9b18f4db5738c017
axelit19 <axelit19@ciencias.unam.mx>
2026-04-19 15:55:40 -0600

    Curso controller - Modificacion

--- controllers/curso_controller.py
+++ controllers/curso_controller.py
@@ -1,8 +1,42 @@
 import os
 from flask import Blueprint, request, redirect, flash, session, current_app, url_for, render_template
+from werkzeug.utils import secure_filename
+from dao.recurso_dao import RecursoDao
 from dao.curso_dao import CursoDao
 
-curso_bp = Blueprint('curso', __name__)
+recursos_bp = Blueprint('recursos', __name__)
+
+
+@recursos_bp.route('/subir_recurso/<int:id_curso>', methods=['GET', 'POST'])
+def subir_recurso(id_curso):
+    if 'usuario' not in session:
+        return redirect(url_for('auth.iniciar_sesion'))
+
+    if request.method == 'GET':
+        
+        return render_template('RegistroUsuarioIH.html', id_curso=id_curso)
+
+    if request.method == 'POST':
+        archivo = request.files.get('archivo')
+        nombre_recurso = request.form.get('nombre_recurso')
+        descripcion = request.form.get('descripcion')
+
+        if archivo and archivo.filename != '':
+            filename = secure_filename(archivo.filename)
+
+            if not os.path.exists(current_app.config['UPLOAD_FOLDER']):
+                os.makedirs(current_app.config['UPLOAD_FOLDER'])
+
+            ruta_guardado = os.path.join(current_app.config['UPLOAD_FOLDER'], filename)
+            archivo.save(ruta_guardado)
+
+            RecursoDao.guardar_recurso(nombre_recurso, filename, descripcion, id_curso)
+
+            flash('Recurso guardado con éxito', 'success')
+            return redirect(url_for('docente.tablero_docente'))
+        else:
+            flash('Archivo no encontrado o inválido', 'danger')
+            return redirect(url_for('recursos.subir_recurso', id_curso=id_curso))
 
 
 
